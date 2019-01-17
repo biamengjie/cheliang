c05b8b51e13d1722524e986d0202e31825cca050
Your Name <you@example.com>
2019-01-17 19:13:45 +0800

    构建程序基本框架

--- main.c
+++ main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
-
+struct Point{
+    int x;
+    int y;
+};
 int main(){
+    int index=0;
+    struct Point bbb[100];
+printf("huanyingjinruzhuangchangguanlixitong:\n");
+    while(1){
+        printf("1---tianjiacheliangweizhi:\n");
+        printf("2---shanchucheliangweizhi:\n");
+        printf("3---xianshiquanbudecheliangweizhi:\n");
+        printf("4---fanhuiliwoweizhizuijindecheliang:\n");
+        printf("5---chengxujieshu:\n");
+        printf("qingshuruxiangyingdegonggnengbianhao:\n");
+        int code;
+        scanf("%d",&code);
+        if(code==1){
 
-    printf("hello world!");
+        }
+        if(code==2){
+            
+        }
+        if(code==3){
+            
+        }
+        if(code==4){
+            
+        }
+        if(code==5){
+            
+        }
+    }
 
     return 0;
 
